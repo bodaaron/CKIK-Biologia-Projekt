15edc3306ae249ab66fbfe530e93916e3fac3dfc
TibiAHegyről... <99481995+emoditibi@users.noreply.github.com>
2025-02-10 18:55:47 +0100

    Create ResetPasswordView.vue

---
+++ src/views/ResetPasswordView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>Jelszó visszaállítása</h2>
+    <form @submit.prevent="handlePasswordReset">
+      <div>
+        <label for="newPassword">Új jelszó:</label>
+        <input type="password" id="newPassword" v-model="newPassword" required />
+      </div>
+      <div>
+        <label for="confirmPassword">Jelszó megerősítése:</label>
+        <input type="password" id="confirmPassword" v-model="confirmPassword" required />
+      </div>
+      <button type="submit">Jelszó visszaállítása</button>
+    </form>
+  </div>
+</template>
+
+<script></script>
