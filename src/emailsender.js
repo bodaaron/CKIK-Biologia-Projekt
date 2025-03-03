4d8c062c8cf9334b78333b42a41cdf0d6e0d5110
TibiAHegyről... <99481995+emoditibi@users.noreply.github.com>
2025-03-03 06:53:53 +0100

    Update emailsender.js

--- src/emailsender.js
+++ src/emailsender.js
@@ -1,5 +1,6 @@
 const nodemailer = require('nodemailer')
 const crypto = require('crypto')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 
 const transporter = nodemailer.createTransport({
@@ -10,13 +11,13 @@ const transporter = nodemailer.createTransport({
   },
 })
 
-function generateToken() {
-  return crypto.randomBytes(32).toString('hex')
+function generateToken(email) {
+  return jwt.sign({ email }, process.env.JWT_KEY, { expiresIn: '1h' })
 }
 
-function sendMail(toEmail) {
-  return new Promise((resolve, reject) => {
-    const token = generateToken()
+async function sendMail(toEmail) {
+  try {
+    const token = generateToken(toEmail)
     const resetPasswordLink = `http://localhost:5173/jelszo-visszaallitas/${token}`
 
     const mailOptions = {
@@ -31,20 +32,17 @@ function sendMail(toEmail) {
           <div style="text-align: center; margin-top: 20px;">
             <a href="${resetPasswordLink}" style="background-color: #4CAF50; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;">Jelszó változtatás</a>
           </div>
-          <p style="font-size: 16px;">Amennyiben nem ön kérte a jelszó változtatás, hagyja figyelmen kívül ezt az üzenetet.</p>
+          <p style="font-size: 16px;">Amennyiben nem Ön kérte a jelszó változtatást, hagyja figyelmen kívül ezt az üzenetet.</p>
           <p style="font-size: 16px;">Üdvözlettel,<br>A Biology Webpage Csapata</p>
         </div>
       `,
     }
 
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        reject(error)
-      } else {
-        resolve(info.response)
-      }
-    })
-  })
+    const info = await transporter.sendMail(mailOptions)
+    return info.response
+  } catch (error) {
+    throw new Error(`Email küldése sikertelen: ${error.message}`)
+  }
 }
 
 module.exports = sendMail
