a8c260c63376ac33973862f838436c9afe168a36
TibiAHegyről... <99481995+emoditibi@users.noreply.github.com>
2025-03-03 06:52:52 +0100

    Update ResetPasswordView.vue

--- src/views/ResetPasswordView.vue
+++ src/views/ResetPasswordView.vue
@@ -1,3 +1,47 @@
+<script lang="ts">
+import { jwtDecode } from 'jwt-decode'
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      newPassword: '',
+      confirmPassword: '',
+      email: '',
+    }
+  },
+  created() {
+    const token = this.$route.params.token
+    try {
+      const decoded = jwtDecode(token)
+      this.email = decoded.email
+      console.log(decoded)
+    } catch (error) {
+      console.error('Hibás token!', error)
+    }
+  },
+  methods: {
+    async handlePasswordReset() {
+
+      if (this.newPassword !== this.confirmPassword) {
+        alert('A jelszavak nem egyeznek!')
+        return
+      }
+
+
+      try {
+        const response = await axios.post('http://localhost:3000/api/reset-password', {
+          email: this.email,
+          newPassword: this.newPassword,
+        })
+        alert(response.data.message)
+      } catch (error) {
+        console.error('Hiba történt a jelszó visszaállításakor:', error)
+      }
+    },
+  },
+}
+</script>
 <template>
   <div>
     <h2>Jelszó visszaállítása</h2>
@@ -14,5 +58,3 @@
     </form>
   </div>
 </template>
-
-<script></script>
