afee97ca1e459465e76f20118da2609fad84ca77
biology <biologywebpageckik@gmail.com>
2026-02-02 13:14:37 +0000

    server változtatások

--- src/database/dbContext.js
+++ src/database/dbContext.js
@@ -13,7 +13,7 @@ const sequelize = new Sequelize
     process.env.DB_PASSWORD,
 
     {
-        host: process.env.DB_HOST,
+        host: "localhost",
         dialect: "mysql",
         logging: false,
     }
